0144f822037352b4c0d951f3b38125084e27dad2
Dominic_SE <105536723+SE-Dominic@users.noreply.github.com>
2026-03-09 17:49:20 +0000

    cleaned up code.

--- Lab 2/PriceFilter.java
+++ Lab 2/PriceFilter.java
@@ -3,7 +3,7 @@ public class PriceFilter implements FilterIF{
     @Override 
     public ArrayList<Product> meetCriteria(ArrayList<Product> list) {
         ArrayList<Product> filteredList = new ArrayList<>();
-        final double filterValue = 15.00;
+        final double filterValue = 25.00;
         
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getPrice() <= filterValue) {
