73caac7f463d1a07e1fb44613099388ecb1e8cf0
Dominic_SE <105536723+SE-Dominic@users.noreply.github.com>
2026-03-09 23:17:18 +0000

    moved problem 1 into its own folder.

--- Lab 2/Question1/Main.java
+++ Lab 2/Question1/Main.java
@@ -8,9 +8,9 @@ class Main {
     public static void main(String [] args) {
         Product p1 = new Product("123", "Soap", 14.99, "general", "lifestyle", 3);
         Product p2 = new Product("456", "Hair gel", 30.00, "costco", "lifestyle", 4);
-        Product p3 = new Product("789", "Water", 3.99, "costco", "consumables", 2);
+        Product p3 = new Product("789", "Water", 3.99, "costco", "consumables", 4);
         Product p4 = new Product("652", "Soda", 1.99, "general", "consumables", 2);
-        Product p5 = new Product("683", "Computer", 200.99, "best buy", "electronics", 2);
+        Product p5 = new Product("683", "Computer", 200.99, "best buy", "electronics", 4);
         Product p6 = new Product("139", "Shampoo", 8.99, "general", "lifestyle", 2);
         Product p7 = new Product("471", "Headphones", 57.99, "best buy", "electronics", 2);
         ArrayList<Product> list = new ArrayList<>();
